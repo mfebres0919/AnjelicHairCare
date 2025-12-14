2bec91e2d715e7070e02fef80c718c57a7ff7e8c
Miguelle Febres <guellefeb21@gmail.com>
2025-12-14 15:30:57 -0500

    update

--- main.js
+++ main.js
@@ -74,3 +74,15 @@ window.addEventListener('load', function () {
         item.addEventListener('click', onClick)
         }
                                 
+
+
+// FAQ Accordion functionality
+        const faqItems = document.querySelectorAll('.faq-item');
+
+faqItems.forEach(item => {
+  const question = item.querySelector('.faq-question');
+
+  question.addEventListener('click', () => {
+    item.classList.toggle('active');
+  });
+});
